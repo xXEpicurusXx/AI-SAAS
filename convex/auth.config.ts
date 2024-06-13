93e107914b24e4d981218181f8ef378aadfbd7ad
Cameron <cameron_mckeel@yahoo.com>
2024-06-13 10:55:03 +0900

    adding convex and clerk to the application.

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://steady-elf-38.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  
+  export default authConfig;
