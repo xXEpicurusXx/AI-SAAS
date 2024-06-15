426de76febc6aa93ad339f96ff5dd48aae079188
Cameron Mckeel <117554357+xXEpicurusXx@users.noreply.github.com>
2024-06-15 10:58:49 +0900

    Implementing generation pages and started working on implementing AI API

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -13,70 +13,74 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
-import { cn } from "@/lib/utils"
-import { useState } from "react"
-import { Textarea } from "@/components/ui/textarea"
-import GeneratePodcast from "@/components/GeneratePodcast"
-import GenerateThumbnail from "@/components/GenerateThumbnail"
-import { Loader } from "lucide-react"
-import { Id } from "@/convex/_generated/dataModel"
-import { useToast } from "@/components/ui/use-toast"
-import { useMutation } from "convex/react"
-import { api } from "@/convex/_generated/api"
-import { useRouter } from "next/navigation"
+} from "@/components/ui/select";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+import { Textarea } from "@/components/ui/textarea";
+import GeneratePodcast from "@/components/GeneratePodcast";
+import GenerateThumbnail from "@/components/GenerateThumbnail";
+import { Loader } from "lucide-react";
+import { Id } from "@/convex/_generated/dataModel";
+import { useToast } from "@/components/ui/use-toast";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useRouter } from "next/navigation";
 
-const voiceCategories = ['alloy', 'shimmer', 'nova', 'echo', 'fable', 'onyx'];
+const voiceCategories = ["alloy", "shimmer", "nova", "echo", "fable", "onyx"];
 
 const formSchema = z.object({
   podcastTitle: z.string().min(2),
   podcastDescription: z.string().min(2),
-})
+});
 
 const CreatePodcast = () => {
-  const router = useRouter()
-  const [imagePrompt, setImagePrompt] = useState('');
-  const [imageStorageId, setImageStorageId] = useState<Id<"_storage"> | null>(null)
-  const [imageUrl, setImageUrl] = useState('');
-  
-  const [audioUrl, setAudioUrl] = useState('');
-  const [audioStorageId, setAudioStorageId] = useState<Id<"_storage"> | null>(null)
+  const router = useRouter();
+  const [imagePrompt, setImagePrompt] = useState("");
+  const [imageStorageId, setImageStorageId] = useState<Id<"_storage"> | null>(
+    null
+  );
+  const [imageUrl, setImageUrl] = useState("");
+
+  const [audioUrl, setAudioUrl] = useState("");
+  const [audioStorageId, setAudioStorageId] = useState<Id<"_storage"> | null>(
+    null
+  );
   const [audioDuration, setAudioDuration] = useState(0);
-  
+
   const [voiceType, setVoiceType] = useState<string | null>(null);
-  const [voicePrompt, setVoicePrompt] = useState('');
-  
+  const [voicePrompt, setVoicePrompt] = useState("");
+
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const createPodcast = useMutation(api.podcasts.createPodcast)
+  const createPodcast = useMutation(api.podcasts.createPodcast);
 
-  const { toast } = useToast()
+  const { toast } = useToast();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       podcastTitle: "",
       podcastDescription: "",
     },
-  })
- 
+  });
+
   async function onSubmit(data: z.infer<typeof formSchema>) {
     try {
       setIsSubmitting(true);
-      if(!audioUrl || !imageUrl || !voiceType) {
+      if (!audioUrl || !imageUrl || !voiceType) {
         toast({
-          title: 'Please generate audio and image',
-        })
+          title: "Please generate audio and image",
+        });
         setIsSubmitting(false);
-        throw new Error('Please generate audio and image')
+        throw new Error("Please generate audio and image");
       }
 
       const podcast = await createPodcast({
@@ -91,16 +95,16 @@ const CreatePodcast = () => {
         audioDuration,
         audioStorageId: audioStorageId!,
         imageStorageId: imageStorageId!,
-      })
-      toast({ title: 'Podcast created' })
+      });
+      toast({ title: "Podcast created" });
       setIsSubmitting(false);
-      router.push('/')
+      router.push("/");
     } catch (error) {
       console.log(error);
       toast({
-        title: 'Error',
-        variant: 'destructive',
-      })
+        title: "Error",
+        variant: "destructive",
+      });
       setIsSubmitting(false);
     }
   }
@@ -110,16 +114,25 @@ const CreatePodcast = () => {
       <h1 className="text-20 font-bold text-white-1">Create Podcast</h1>
 
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="mt-12 flex w-full flex-col">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mt-12 flex w-full flex-col"
+        >
           <div className="flex flex-col gap-[30px] border-b border-black-5 pb-10">
             <FormField
               control={form.control}
               name="podcastTitle"
               render={({ field }) => (
                 <FormItem className="flex flex-col gap-2.5">
-                  <FormLabel className="text-16 font-bold text-white-1">Title</FormLabel>
+                  <FormLabel className="text-16 font-bold text-white-1">
+                    Title
+                  </FormLabel>
                   <FormControl>
-                    <Input className="input-class focus-visible:ring-offset-rose-500" placeholder="PodPilot Pro Prodcast" {...field} />
+                    <Input
+                      className="input-class focus-visible:ring-offset-rose-500"
+                      placeholder="PodPilot Pro Prodcast"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage className="text-white-1" />
                 </FormItem>
@@ -132,18 +145,29 @@ const CreatePodcast = () => {
               </Label>
 
               <Select onValueChange={(value) => setVoiceType(value)}>
-                <SelectTrigger className={cn('text-16 w-full border-none bg-black-1 text-gray-1 focus-visible:ring-offset-rose^500')}>
-                  <SelectValue placeholder="Select AI Voice" className="placeholder:text-gray-1 " />
+                <SelectTrigger
+                  className={cn(
+                    "text-16 w-full border-none bg-black-1 text-gray-1 focus-visible:ring-offset-rose-500"
+                  )}
+                >
+                  <SelectValue
+                    placeholder="Select AI Voice"
+                    className="placeholder:text-gray-1 "
+                  />
                 </SelectTrigger>
                 <SelectContent className="text-16 border-none bg-black-1 font-bold text-white-1 focus:ring-rose-500">
                   {voiceCategories.map((category) => (
-                    <SelectItem key={category} value={category} className="capitalize focus:bg-rose-500">
+                    <SelectItem
+                      key={category}
+                      value={category}
+                      className="capitalize focus:bg-rose-500"
+                    >
                       {category}
                     </SelectItem>
                   ))}
                 </SelectContent>
                 {voiceType && (
-                  <audio 
+                  <audio
                     src={`/${voiceType}.mp3`}
                     autoPlay
                     className="hidden"
@@ -157,9 +181,15 @@ const CreatePodcast = () => {
               name="podcastDescription"
               render={({ field }) => (
                 <FormItem className="flex flex-col gap-2.5">
-                  <FormLabel className="text-16 font-bold text-white-1">Description</FormLabel>
+                  <FormLabel className="text-16 font-bold text-white-1">
+                    Description
+                  </FormLabel>
                   <FormControl>
-                    <Textarea className="input-class focus-visible:ring-offset-rose-500" placeholder="Write a short podcast description" {...field} />
+                    <Textarea
+                      className="input-class focus-visible:ring-offset-rose-500"
+                      placeholder="Write a short podcast description"
+                      {...field}
+                    />
                   </FormControl>
                   <FormMessage className="text-white-1" />
                 </FormItem>
@@ -167,41 +197,44 @@ const CreatePodcast = () => {
             />
           </div>
           <div className="flex flex-col pt-10">
-              <GeneratePodcast 
-                setAudioStorageId={setAudioStorageId}
-                setAudio={setAudioUrl}
-                voiceType={voiceType!}
-                audio={audioUrl}
-                voicePrompt={voicePrompt}
-                setVoicePrompt={setVoicePrompt}
-                setAudioDuration={setAudioDuration}
-              />
+            <GeneratePodcast
+              setAudioStorageId={setAudioStorageId}
+              setAudio={setAudioUrl}
+              voiceType={voiceType!}
+              audio={audioUrl}
+              voicePrompt={voicePrompt}
+              setVoicePrompt={setVoicePrompt}
+              setAudioDuration={setAudioDuration}
+            />
 
-              <GenerateThumbnail 
-               setImage={setImageUrl}
-               setImageStorageId={setImageStorageId}
-               image={imageUrl}
-               imagePrompt={imagePrompt}
-               setImagePrompt={setImagePrompt}
-              />
+            <GenerateThumbnail
+              setImage={setImageUrl}
+              setImageStorageId={setImageStorageId}
+              image={imageUrl}
+              imagePrompt={imagePrompt}
+              setImagePrompt={setImagePrompt}
+            />
 
-              <div className="mt-10 w-full">
-                <Button type="submit" className="text-16 w-full bg-rose-500 py-4 font-extrabold text-white-1 transition-all duration-500 hover:bg-black-1">
-                  {isSubmitting ? (
-                    <>
-                      Submitting
-                      <Loader size={20} className="animate-spin ml-2" />
-                    </>
-                  ) : (
-                    'Submit'
-                  )}
-                </Button>
-              </div>
+            <div className="mt-10 w-full">
+              <Button
+                type="submit"
+                className="text-16 w-full bg-rose-500 py-4 font-extrabold text-white-1 transition-all duration-500 hover:bg-black-1"
+              >
+                {isSubmitting ? (
+                  <>
+                    Submitting
+                    <Loader size={20} className="animate-spin ml-2" />
+                  </>
+                ) : (
+                  "Submit"
+                )}
+              </Button>
+            </div>
           </div>
         </form>
       </Form>
     </section>
-  )
-}
+  );
+};
 
-export default CreatePodcast
+export default CreatePodcast;
